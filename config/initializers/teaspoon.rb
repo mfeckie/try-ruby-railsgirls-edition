d685bdbe6405196f5157fcfc20aedf13a9aa1706
Martin Feckie <mfeckie@gmail.com>
2014-03-26 21:39:07 +0800

    CSS for teaspoon test runner

--- config/initializers/teaspoon.rb
+++ config/initializers/teaspoon.rb
@@ -46,7 +46,7 @@ Teaspoon.setup do |config|
 
     # If you want to change how Teaspoon looks, or include your own stylesheets you can do that here. The default is the
     # stylesheet for the HTML reporter.
-    suite.stylesheets = ["teaspoon", "teaspoon_custom"]
+    suite.stylesheets = ["teaspoon", "teaspoon_custom", "jquery-terminal", "application"]
 
     # When running coverage reports, you probably want to exclude libraries that you're not testing.
     # Accepts an array of filenames or regular expressions. The default is to exclude assets from vendors or gems.
