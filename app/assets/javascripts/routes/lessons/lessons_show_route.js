e57ab9e4b25b411b0b6791e8199c959ff81a5f2b
Martin Feckie <mfeckie@gmail.com>
2014-04-07 15:34:00 +0800

    Remove some cruft

--- app/assets/javascripts/routes/lessons/lessons_show_route.js
+++ app/assets/javascripts/routes/lessons/lessons_show_route.js
@@ -1,11 +1,5 @@
 TryRubyRailsGirls.LessonsShowRoute = Ember.Route.extend({
     model: function (params) {
         return this.store.find('lesson', params.id );
-//        return {
-//            lesson_number: 1,
-//            title: 'Working with Strings',
-//            body: 'Strings in Ruby are awesome',
-//            initialCode: 'puts "Hello Friend!"'
-//        }
     }
-})
+});
