823021ab3e9bcf89fbdbc4274c9cad2f7e8f80f7
Martin Feckie <mfeckie@gmail.com>
2014-04-07 15:33:24 +0800

    Add FixtureAdapter to allow testing

--- spec/javascripts/integration/lessons/show_spec.js
+++ spec/javascripts/integration/lessons/show_spec.js
@@ -1,5 +1,11 @@
-module('Individual lessons');
+module('Individual lessons', {
+    setup: function() {
 
+    },
+    teardown: function() {
+        resetFixtures();
+    }
+});
 
 test('Visiting an individual lesson', function () {
     visit('/lessons/1');
@@ -7,6 +13,9 @@ test('Visiting an individual lesson', function () {
         elementHasText('.panel', 'Lesson 1');
         elementHasText('.panel', 'Working with Strings');
         elementHasText('.panel', 'Strings in Ruby are');
+        var editorText = window.editor.getValue();
+        var matcher = /puts \"Hello Friend!\"/;
+        ok(matcher.test(editorText), editorText);
     });
 });
 
--- spec/javascripts/spec_helper.js
+++ spec/javascripts/spec_helper.js
@@ -10,3 +10,26 @@ TryRubyRailsGirls.rootElement = "#ember-testing";
 TryRubyRailsGirls.setupForTesting();
 TryRubyRailsGirls.injectTestHelpers();
 
+var resetFixtures = function () {
+    TryRubyRailsGirls.ApplicationAdapter = DS.FixtureAdapter;
+
+    TryRubyRailsGirls.Lesson.FIXTURES = [
+        {
+            id: 1,
+            lesson_number: 1,
+            title: 'Working with Strings',
+            body: 'Strings in Ruby are',
+            initialCode: 'puts "Hello Friend!"'
+
+        },
+        {
+            id: 2,
+            lesson_number: 2,
+            title: 'Working with numbers',
+            body: 'Numbers in Runy are',
+            initialCode: 'puts 1 +1'
+        }
+    ];
+};
+
+resetFixtures();
